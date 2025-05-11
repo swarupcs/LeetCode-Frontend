1684c669de36d2d370315a46bcd9db24bb4db395
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-11 15:00:38 +0530

    Add CreateProblemPage and ProblemPage components to handle problem creation and listing

---
+++ src/Pages/ProblemSet/CreateProblemPage.jsx
@@ -0,0 +1,10 @@
+import CreateProblem from '@/components/CreateProblem/CreateProblem'
+import React from 'react'
+
+export const CreateProblemPage = () => {
+  return (
+    <>
+    <CreateProblem/>
+    </>
+  )
+}
---
+++ src/Pages/ProblemSet/ProblemPage.jsx
@@ -0,0 +1,8 @@
+import AllProblem from '@/components/ProblemSet/AllProblem'
+import React from 'react'
+
+export const ProblemPage = () => {
+  return (
+    <><AllProblem/></>
+  )
+}
