0bc262ac5e010054096d9be927ec93a251363bde
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-24 17:21:59 +0530

    feat: Implement getAllSheetDetailsRequest API for fetching sheet details

---
+++ src/apis/getAllSheetDetails/index.js
@@ -0,0 +1,11 @@
+import axios from '@/config/axiosConfig';
+
+export const getAllSheetDetailsRequest = async () => {
+  try {
+    const response = await axios.get('/sheets');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error.response.data;
+  }
+}
