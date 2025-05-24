36e8c118cb99d857846f614f2f1b086240770914
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-24 09:15:55 +0530

    feat: Implement updateProblemDetails API for updating problem information

---
+++ src/apis/updateProblemDetails/index.js
@@ -0,0 +1,21 @@
+import axios from "@/config/axiosConfig";
+
+export const updateProblemDetails = async (
+  problemId,
+  problemNumber,
+  problem,
+  testCases
+) => {
+    try {
+        const response = await axios.put(`/problems/update-problem/${problemId}`, {
+            problemNumber,
+            problem,
+            testCases,
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log("error", error);
+        throw error.response.data;
+    }
+};
