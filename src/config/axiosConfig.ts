c0269cfb8730988b83c24185203ee133bdc2f463
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-02-22 11:07:31 +0530

    setup axios

---
+++ src/config/axiosConfig.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
